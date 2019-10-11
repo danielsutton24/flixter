ebedc7b1a0c92ed8f278785fe234f24d6758d12a
Daniel <sutton.daniel.ds@gmail.com>
2019-10-11 18:05:25 +0000

    payments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,13 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to current_lesson.section.course, alert: 'You must be enrolled to view course content'
+    end
+  end
+  
+
   def show
   end
 
